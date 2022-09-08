99957d95775c1ae8b0475cfa6bf2121bcec9e02c
Vlad Foroshcha <vladfo2003@gmail.com>
2022-09-08 17:51:23 +0300

    feat: vite, eslint and prettier config

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,16 @@ import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ command, mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    plugins: [react()],
+    define: {
+      PORT: env.PORT
+    },
+    envDir: 'env',
+    server: {
+      port: 4545
+    }
+  }
 })
